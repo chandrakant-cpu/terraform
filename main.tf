234fa6fa73f0fcda7ebb285b94a7156ca58efe86
chandrakant-cpu <52148044+chandrakant-cpu@users.noreply.github.com>
2020-05-11 04:52:34 -0400

    this is assignment1
    this is assignment1

---
+++ main.tf
@@ -0,0 +1,8 @@
+data "docker_registry_image" "nginx" {	
+name = "nginx:latest"	
+}	
+ 	 	
+resource "docker_image" "nginx" {	
+ name = "${data.docker_registry_image.nginx.name}"	
+ pull_triggers = ["${data.docker_registry_image.nginx.name}"] 	
+ }	
